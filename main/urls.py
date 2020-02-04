cd90ed9e5fd888ad9ebc19ab4eaeea6a0e4fd368
sergeev-av <sega21rus98@mail.ru>
2020-02-04 14:53:09 +0300

    Начальные шаблоны

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.static import serve
 
 from medicine_centre import settings
 from . import views
@@ -9,7 +7,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name="index"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
