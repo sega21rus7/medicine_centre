92657bd88b7de67e77404a2a9db9acf6f1cc28ec
sergeev-av <sega21rus98@mail.ru>
2020-02-04 16:22:36 +0300

    Доб/Ред User в админке

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
+from user_profile.forms import CustomerUserCreationForm, CustomerUserChangeForm
 from user_profile.models import CustomerUser
 
-admin.site.register(CustomerUser)
+
+class CustomerUserAdmin(UserAdmin):
+    add_form = CustomerUserCreationForm
+    form = CustomerUserChangeForm
+    model = CustomerUser
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('email', 'is_staff', 'is_superuser', 'is_active', 'groups')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2',)}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(CustomerUser, CustomerUserAdmin)
