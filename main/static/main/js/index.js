4fee3550f8f25e22f03f9299ef6054671d724d60
sergeev-av <sega21rus98@mail.ru>
2020-02-05 18:12:25 +0300

    Форма регистрации в модальном окне

--- main/static/main/js/index.js
+++ main/static/main/js/index.js
@@ -8,10 +8,11 @@ $(function () {
             type: 'get',
             dataType: 'json',
             beforeSend: function () {
-                $('.modal-sign-up').modal('show');
+
             },
             success: function (data) {
-                $('.modal-sign-up .modal-content').html(data.html);
+                $('.modal-sign-up').modal('show');
+                $('.modal-sign-up .modal-content').html(data.html_form);
             }
         })
     })
