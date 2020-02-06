b0881df0f8d1c91f24f414c420b6c1e41c85c48c
sergeev-av <sega21rus98@mail.ru>
2020-02-06 20:38:47 +0300

    Начинаем переписывать фронт входа

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,7 +1,5 @@
-from django.contrib.auth import authenticate, login, logout
-from django.http import JsonResponse
-from django.shortcuts import redirect
-from django.template.loader import render_to_string
+from django.contrib.auth import logout
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views.generic.base import View
 
@@ -20,22 +18,21 @@ class SignUpView(View):
 
     def get(self, request):
         context = {'form': self.form_class}
-        html_form = render_to_string(template_name=self.template_name, context=context, request=request)
-        return JsonResponse({'html_form': html_form})
+        return render(request=request, template_name=self.template_name, context=context)
 
-    def post(self, request):
-        data = {}
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            email = request.POST.get('email')
-            password = request.POST.get('password')
-            user = form.save()
-            if user:
-                authenticate(email=email, password=password)
-                login(request, user)
-        else:
-            data['errors'] = form.errors
-        return JsonResponse(data)
+    # def post(self, request):
+    #     data = {}
+    #     form = self.form_class(request.POST)
+    #     if form.is_valid():
+    #         email = request.POST.get('email')
+    #         password = request.POST.get('password')
+    #         user = form.save()
+    #         if user:
+    #             authenticate(email=email, password=password)
+    #             login(request, user)
+    #     else:
+    #         data['errors'] = form.errors
+    #     return JsonResponse(data)
 
 
 class SignInView(View):
@@ -43,20 +40,18 @@ class SignInView(View):
     template_name = 'user_profile/sign_in.html'
 
     def get(self, request):
-        data = {}
         context = {'form': self.form_class}
-        data['html_form'] = render_to_string(template_name=self.template_name, context=context, request=request)
-        return JsonResponse(data)
+        return render(request=request, template_name=self.template_name, context=context)
 
-    def post(self, request):
-        data = {}
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            email = request.POST.get('email')
-            password = request.POST.get('password')
-            user = authenticate(email=email, password=password)
-            if user and user.is_active:
-                login(request, user)
-        else:
-            data['errors'] = form.errors
-        return JsonResponse(data)
+    # def post(self, request):
+    #     data = {}
+    #     form = self.form_class(request.POST)
+    #     if form.is_valid():
+    #         email = request.POST.get('email')
+    #         password = request.POST.get('password')
+    #         user = authenticate(email=email, password=password)
+    #         if user and user.is_active:
+    #             login(request, user)
+    #     else:
+    #         data['errors'] = form.errors
+    #     return JsonResponse(data)
