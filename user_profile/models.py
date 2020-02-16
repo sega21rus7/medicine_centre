99a50f8f7bb35f8c8dee2c37c0cb4a61c8782dfc
sergeev-av <sega21rus98@mail.ru>
2020-02-16 14:32:44 +0300

    email - unique

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class CustomerUser(AbstractUser):
+    email = models.EmailField(_('Email address'), unique=True)
+
     # user = models.ForeignKey(settings.AUTH_USER_MODEL) - в дальнейшем используем так
     class Meta:
         verbose_name = 'пользователь'
