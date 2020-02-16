da8436bb731399ceac973b961d4612accf1bf72f
sergeev-av <sega21rus98@mail.ru>
2020-02-16 14:31:48 +0300

    сокращение кода

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -7,19 +7,18 @@ User = get_user_model()
 class SignUpForm(UserCreationForm):
     class Meta():
         model = User
-        fields = ('username', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['placeholder'] = 'Введите логин'
-        self.fields['password1'].widget.attrs['placeholder'] = 'Придумайте пароль'
+        self.fields['username'].widget.attrs['placeholder'] = 'Придумайте логин'
+        self.fields['email'].widget.attrs['placeholder'] = 'Введите email'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Введите пароль'
         self.fields['password2'].widget.attrs['placeholder'] = 'Подтвердите пароль'
-        self.fields['username'].label = ''
-        self.fields['password1'].label = ''
-        self.fields['password2'].label = ''
-        self.fields['username'].help_text = ''
-        self.fields['password1'].help_text = ''
-        self.fields['password2'].help_text = ''
+
+        for field in self.fields.values():
+            field.label = ''
+            field.help_text = ''
 
 
 class SignInForm(AuthenticationForm):
@@ -27,5 +26,5 @@ class SignInForm(AuthenticationForm):
         super(SignInForm, self).__init__(self, *args, **kwargs)
         self.fields['username'].widget.attrs['placeholder'] = 'Введите логин'
         self.fields['password'].widget.attrs['placeholder'] = 'Введите пароль'
-        self.fields['username'].label = ''
-        self.fields['password'].label = ''
+        for field in self.fields.values():
+            field.label = ''
