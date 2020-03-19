4c65d053d7a6efdf5040fbd0f3cea57b85ec6398
sergeev-av <sega21rus98@mail.ru>
2020-03-19 13:33:58 +0300

    База

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# admin.site.register(model)
+from .models import Doctor, Nurse, Passport, InsurancePolicy, Patient, News
+
+admin.site.register(Doctor)
+admin.site.register(Nurse)
+admin.site.register(Passport)
+admin.site.register(InsurancePolicy)
+admin.site.register(Patient)
+admin.site.register(News)
