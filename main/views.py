c7b7fa4b4ee40b005489064b7d0db22d1a84a996
sergeev-av <sega21rus98@mail.ru>
2020-03-30 14:39:23 +0300

    последние 3 новости

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import DetailView
-from django.views.generic.base import TemplateView, View
+from django.views.generic.base import TemplateView
 
 from .models import News, Doctor, BigNews
 
@@ -9,7 +9,7 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['news'] = News.objects.all()
+        context['news'] = News.objects.all()[:3]
         context['doctors'] = Doctor.objects.all()
         context['big_news'] = BigNews.objects.all()
         return context
