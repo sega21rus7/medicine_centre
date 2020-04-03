87d8e239c0608823b24d2f6bb4c25c16f1d955ae
sergeev-av <sega21rus98@mail.ru>
2020-04-03 16:07:51 +0300

    Форма поиска только в новостях

--- news/views.py
+++ news/views.py
@@ -1,16 +1,28 @@
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
 from .models import News, BigNews
 
 
-class NewsView(ListView):
+class SearchListView(ListView):
+    def get_queryset(self):
+        search = self.request.GET.get('search', '')
+        if search:
+            news = self.model.objects.filter(Q(title__icontains=search) |
+                                             Q(content__contains=search))
+        else:
+            news = self.model.objects.all()
+        return news
+
+
+class NewsView(SearchListView):
     model = News
     template_name = 'news/news_list.html'
     context_object_name = 'news'
     paginate_by = 6
 
 
-class BigNewsView(ListView):
+class BigNewsView(SearchListView):
     model = BigNews
     template_name = 'news/big_news_list.html'
     context_object_name = 'big_news'
