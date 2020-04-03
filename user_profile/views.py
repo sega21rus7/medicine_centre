1cbf3afa8855e5a45c50b4c4bfc6281721cce423
sergeev-av <sega21rus98@mail.ru>
2020-04-03 13:06:33 +0300

    Лишний импорт

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import logout, authenticate, login
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views.generic.base import View, TemplateView
