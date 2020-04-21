1269c7d8a4222ea7d6b231dd089927d8febbef4b
sergeev-av <sega21rus98@mail.ru>
2020-04-21 16:00:30 +0300

    -старая авторизация

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,6 +6,5 @@ app_name = 'user_profile'
 
 urlpatterns = [
     url(r'sign_up/$', views.SignUpView.as_view(), name="sign_up"),
-    url(r'sign_in/$', views.SignInView.as_view(), name="sign_in"),
     url(r'sign_out/$', views.SignOutView.as_view(), name="sign_out"),
 ]
