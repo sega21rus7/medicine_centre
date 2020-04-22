4c2af21129d3966e0178b27cc58139d4403b249e
sergeev-av <sega21rus98@mail.ru>
2020-04-22 14:35:08 +0300

    Еще шаблоны рег.

--- backend/medicine_centre/medicine_centre/settings.py
+++ backend/medicine_centre/medicine_centre/settings.py
@@ -144,3 +144,4 @@ LOGIN_URL = '/accounts/login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
