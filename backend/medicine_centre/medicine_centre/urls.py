cf616c7f4922f4dbb089ea391f7987b48260cbe9
sergeev-av <sega21rus98@mail.ru>
2020-04-25 12:58:57 +0300

    Установка djangorest

--- backend/medicine_centre/medicine_centre/urls.py
+++ backend/medicine_centre/medicine_centre/urls.py
@@ -1,8 +1,7 @@
-from django.urls import include, path
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-
-from django.conf import settings
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('main.urls')),
@@ -12,6 +11,7 @@ urlpatterns = [
     path('client/', include('client.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
