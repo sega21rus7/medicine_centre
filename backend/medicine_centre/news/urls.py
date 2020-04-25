ef1348ec61562b2a88b99e0691b67dfc13c17439
sergeev-av <sega21rus98@mail.ru>
2020-04-25 13:02:55 +0300

    уже подключено в главном urls

--- backend/medicine_centre/news/urls.py
+++ backend/medicine_centre/news/urls.py
@@ -16,5 +16,3 @@ urlpatterns = [
 
     path('api/', include('news.api.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
