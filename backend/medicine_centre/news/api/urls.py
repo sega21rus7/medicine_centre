dfece3e8d4d275014e9a5da04b37560924b3ab96
sergeev-av <sega21rus98@mail.ru>
2020-04-26 12:01:34 +0300

    без _api в url-ах

--- backend/medicine_centre/news/api/urls.py
+++ backend/medicine_centre/news/api/urls.py
@@ -5,13 +5,13 @@ from .views import ArticleListView, ArticleDetailView, \
     ArticleCommentListView, TagDetailView
 
 urlpatterns = [
-    path('article_list/', ArticleListView.as_view(), name='article_list_api'),
-    path('article_detail/<slug>/', ArticleDetailView.as_view(), name='article_detail_api'),
+    path('article_list/', ArticleListView.as_view(), name='article_list'),
+    path('article_detail/<slug>/', ArticleDetailView.as_view(), name='article_detail'),
 
     path('news_list/', NewsListView.as_view(), name='news_list_api'),
-    path('news_detail/<slug>/', NewsDetailView.as_view(), name='news_detail_api'),
+    path('news_detail/<slug>/', NewsDetailView.as_view(), name='news_detail'),
 
-    path('comment_list/', ArticleCommentListView.as_view(), name='comment_list_api'),
+    path('comment_list/', ArticleCommentListView.as_view(), name='comment_list'),
 
-    path('tag_detail/<slug>/', TagDetailView.as_view(), name='tag_detail_api'),
+    path('tag_detail/<slug>/', TagDetailView.as_view(), name='tag_detail'),
 ]
