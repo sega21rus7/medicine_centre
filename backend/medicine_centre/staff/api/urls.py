1ea87976177f5aee61032431bc6d018d11668d05
sergeev-av <sega21rus98@mail.ru>
2020-04-26 13:23:00 +0300

    пока что различаем urls rest

--- backend/medicine_centre/staff/api/urls.py
+++ backend/medicine_centre/staff/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import DoctorListView, DoctorDetailView
 
 urlpatterns = [
-    path('doctor_list/', DoctorListView.as_view(), name='doctor_list'),
-    path('doctor_detail/<slug>/', DoctorDetailView.as_view(), name='doctor_detail'),
+    path('doctor_list/', DoctorListView.as_view(), name='doctor_list_api'),
+    path('doctor_detail/<slug>/', DoctorDetailView.as_view(), name='doctor_detail_api'),
 ]
