9b4e375c9936d07d58f6193c938d151bb7aae158
sergeev-av <sega21rus98@mail.ru>
2020-04-30 14:22:16 +0300

    пагинация

--- frontend/react-frontend/src/DoctorList/DoctorList.js
+++ frontend/react-frontend/src/DoctorList/DoctorList.js
@@ -9,68 +9,88 @@ class DoctorList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
+      next: null,
+      previous: null,
+      count: 0,
       doctors: [],
+      activePage: 1,
+      paginateCount: 0,
     };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:8000/staff/api/doctor_list/')
+    this.getData();
+  }
+
+  getData = () => {
+    const url = `http://localhost:8000/staff/api/doctor_list/?page=${this.state.activePage}`;
+    axios.get(url)
       .then(response => {
         this.setState({
-          data: response.data,
           doctors: response.data.results,
+          next: response.data.next,
+          previous: response.data.previous,
+          count: response.data.count,
         });
         console.log(response.data);
       })
-  }
-
-  get_first_page = () =>{
-    axios.get('http://localhost:8000/staff/api/doctor_list/?page=1/')
-      .then(response => {
+      .then(() => {
         this.setState({
-          doctors: response.data.results,
+          paginateCount: Math.ceil(this.state.count / 3)
         });
       })
   };
 
-  get_prev_page = () => {
-    axios.get(this.state.data.previous)
-      .then(response => {
-        this.setState({
-          doctors: response.data.results,
-        });
-      })
+  getPagination = () => {
+    let pagination = [];
+    for (let i = 0; i < this.state.paginateCount; i++) {
+      pagination[i] = i + 1;
+    }
+    return pagination;
+  };
+
+  clickPagination = event => {
+    // console.log(event.target.getAttribute('value'));
+    const page = event.target.getAttribute('value') ||
+      event.target.parentElement.getAttribute('value');
+    this.setState({activePage: page}, () => {
+      this.getData();
+    });
   };
 
   render() {
+    const {doctors, next, previous, count} = this.state;
+    const pagination = this.getPagination();
+
     return (
       <div className="DoctorList">
         <Container>
           <h3 className="text-left">Наши специалисты</h3>
           <Row>
-            {this.state.doctors.map((item, index) => (
+            {doctors.map((item, index) => (
                 <DoctorShortItem key={index} item={item} index={index}/>
               )
             )}
           </Row>
+
           <Pagination>
-            <Pagination.First onClick={this.get_first_page}/>
-            <Pagination.Prev/>
-            <Pagination.Item>{1}</Pagination.Item>
-            <Pagination.Ellipsis/>
-
-            <Pagination.Item>{10}</Pagination.Item>
-            <Pagination.Item>{11}</Pagination.Item>
-            <Pagination.Item active>{12}</Pagination.Item>
-            <Pagination.Item>{13}</Pagination.Item>
-            <Pagination.Item disabled>{14}</Pagination.Item>
-
-            <Pagination.Ellipsis/>
-            <Pagination.Item>{20}</Pagination.Item>
-            <Pagination.Next/>
-            <Pagination.Last/>
+            <Pagination.First
+              onClick={this.clickPagination}
+              value={1}
+            />
+            {pagination.map((item, index) => (
+                <Pagination.Item onClick={this.clickPagination}
+                                 key={index} value={item}
+                                 active={item === this.state.activePage}>
+                  {item}
+                </Pagination.Item>
+              )
+            )}
+            <Pagination.Last onClick={this.clickPagination}
+                             value={pagination.length}
+            />
           </Pagination>
+
         </Container>
       </div>
     )
