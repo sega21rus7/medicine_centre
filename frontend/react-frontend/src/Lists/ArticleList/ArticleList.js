c2c0356f31c8517db498cd56564e49978b144dd3
sergeev-av <sega21rus98@mail.ru>
2020-05-01 16:05:47 +0300

    страницы новостей + статей

--- frontend/react-frontend/src/Lists/ArticleList/ArticleList.js
+++ frontend/react-frontend/src/Lists/ArticleList/ArticleList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './ArticleList.css';
+import CommonList from "../CommonList/CommonList";
 
 
 class ArticleList extends React.Component {
   render() {
     return (
       <div className="ArticleList">
-        ArticleList
+        <CommonList title={'Статьи'}
+                    url='http://localhost:8000/news/api/article_list/'
+                    kind={'articles'}
+        />
       </div>
     )
   };
--- frontend/react-frontend/src/Lists/NewsList/NewsList.js
+++ frontend/react-frontend/src/Lists/NewsList/NewsList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './NewsList.css';
+import CommonList from "../CommonList/CommonList";
 
 
 class NewsList extends React.Component {
   render() {
     return (
       <div className="NewsList">
-        NewsList
+        <CommonList title={'Новости'}
+                    url='http://localhost:8000/news/api/news_list/'
+                    kind={'news'}
+        />
       </div>
     )
   };
