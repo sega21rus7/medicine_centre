ffac9e7caaf6d6d504ac7bf836406aa43f073fd2
sergeev-av <sega21rus98@mail.ru>
2020-05-01 16:03:37 +0300

    Обобщенный элемент для списков с пагинацией

---
+++ frontend/react-frontend/src/Lists/CommonList/CommonList.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './CommonList.css';
+import axios from "axios";
+import {Container, Row} from "react-bootstrap";
+import PaginationComponent from "../Pagination/PaginationComponent";
+import DoctorShortItem from "../../MainPage/DoctorShortList/DoctorShortItem/DoctorShortItem";
+import NewsShortItem from "../../MainPage/NewsShortList/NewsShortItem/NewsShortItem";
+import ArticleShortItem from "../../MainPage/ArticleShortList/ArticleShortItem/ArticleShortItem";
+
+
+class CommonList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      next: null,
+      previous: null,
+      count: 0,
+      items: [],
+      paginateCount: 0,
+    };
+    this.paginate_by = 3;
+  }
+
+  componentDidMount() {
+    this.getData(1);
+  }
+
+  getData = (page) => {
+    const url = `${this.props.url}?page=${page}`;
+    axios.get(url)
+      .then(response => {
+        this.setState({
+          items: response.data.results,
+          next: response.data.next,
+          previous: response.data.previous,
+          count: response.data.count,
+        });
+        console.log(response.data);
+      })
+      .then(() => {
+        this.setState({
+          paginateCount: Math.ceil(this.state.count / this.paginate_by)
+        });
+      })
+  };
+
+  render() {
+    const {next, previous, items, paginateCount} = this.state;
+    const {title, kind} = this.props;
+    const row = this.state.items.map((item, index) => {
+        let res = '';
+        if (kind === 'doctors')
+          res = <DoctorShortItem key={index} item={item} index={index}/>;
+        else if (kind === 'news')
+          res = <NewsShortItem key={index} item={item} index={index}/>;
+        else if (kind === 'articles')
+          res = <ArticleShortItem key={index} item={item} index={index}/>;
+        return res;
+      }
+    );
+
+
+    return (
+      <Container>
+        <h3 className="text-left">{title}</h3>
+        <Row>
+          {row}
+        </Row>
+
+        <PaginationComponent items={items}
+                             getData={this.getData}
+                             paginateCount={paginateCount}
+                             next={next}
+                             previous={previous}/>
+
+      </Container>
+    )
+  };
+}
+
+export default CommonList;
--- frontend/react-frontend/src/Lists/DoctorList/DoctorList.js
+++ frontend/react-frontend/src/Lists/DoctorList/DoctorList.js
@@ -1,68 +1,16 @@
 import React from 'react';
 import './DoctorList.css';
-import axios from "axios";
-import {Container, Row} from "react-bootstrap";
-import PaginationComponent from "../Pagination/PaginationComponent";
-import DoctorShortItem from "../../MainPage/DoctorShortList/DoctorShortItem/DoctorShortItem";
+import CommonList from "../CommonList/CommonList";
 
 
 class DoctorList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      next: null,
-      previous: null,
-      count: 0,
-      doctors: [],
-      paginateCount: 0,
-    };
-    this.paginate_by = 3;
-  }
-
-  componentDidMount() {
-    this.getData(1);
-  }
-
-  getData = (page) => {
-    const url = `http://localhost:8000/staff/api/doctor_list/?page=${page}`;
-    axios.get(url)
-      .then(response => {
-        this.setState({
-          doctors: response.data.results,
-          next: response.data.next,
-          previous: response.data.previous,
-          count: response.data.count,
-        });
-        console.log(response.data);
-      })
-      .then(() => {
-        this.setState({
-          paginateCount: Math.ceil(this.state.count / this.paginate_by)
-        });
-      })
-  };
-
   render() {
-    const {next, previous, doctors, paginateCount} = this.state;
-
     return (
       <div className="DoctorList">
-        <Container>
-          <h3 className="text-left">Наши специалисты</h3>
-          <Row>
-            {doctors.map((item, index) => (
-                <DoctorShortItem key={index} item={item} index={index}/>
-              )
-            )}
-          </Row>
-
-          <PaginationComponent items={doctors}
-                               getData={this.getData}
-                               paginateCount={paginateCount}
-                               next={next}
-                               previous={previous}/>
-
-        </Container>
+        <CommonList title={'Наши специалисты'}
+                    url='http://localhost:8000/staff/api/doctor_list/'
+                    kind={'doctors'}
+        />
       </div>
     )
   };
