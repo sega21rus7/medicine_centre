655c89f3c678e90ceb4fd0361dbe15f3dc344b60
sergeev-av <sega21rus98@mail.ru>
2020-05-02 12:30:19 +0300

    функционал логина

--- frontend/react-frontend/src/Auth/SignIn/SignIn.js
+++ frontend/react-frontend/src/Auth/SignIn/SignIn.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import './SignIn.css';
-import {Button, Card, Col, Container, Form, Image, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Dropdown, Form, Image, Row} from "react-bootstrap";
 import image from './sign_in.jpg'
 import {Link} from "react-router-dom";
+import DropdownItem from "react-bootstrap/DropdownItem";
+import axios from "axios";
+import {Redirect} from "react-router";
 
 class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: null,
+      email: null,
+      password: null,
+      isEmailInput: false,
+      token: null,
+      redirect: false,
+    }
+  }
+
+  switchSignInMethod = (event) => {
+    const byEmail = event.target.name === 'byEmail';
+    this.setState({isEmailInput: byEmail});
+  };
+
+  handleInput = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.isEmailInput) {
+      var data = {
+        email: this.state.email,
+        password: this.state.password,
+      }
+    } else {
+      data = {
+        username: this.state.login,
+        password: this.state.password,
+      }
+    }
+    axios.post('http://localhost:8000/rest-auth/login/', data)
+      .then(response => {
+         this.setState({
+           token: response.data,
+           redirect: true,
+         });
+        console.log(response.data);
+      })
+  };
+
   render() {
+    const {redirect, isEmailInput} = this.state;
+    if (redirect) {
+       return <Redirect to='lk/'/>;
+     }
+
+    const passwordInput = <Form.Control className="form-control-user" type="password" name="password"
+                                        placeholder="Пароль" onChange={this.handleInput}/>;
+    const emailInput = <Form.Control className="form-control-user" type="email" name="email"
+                                     placeholder="Email" onChange={this.handleInput}/>;
+    const loginInput = <Form.Control className="form-control-user" type="text" name="login"
+                                     placeholder="Логин" onChange={this.handleInput}/>;
+    const emailOrLoginInput = isEmailInput ? emailInput : loginInput;
+
     return (
       <Container className="SignIn">
         <Card className="o-hidden border-0 shadow-lg mt-4">
@@ -22,28 +82,39 @@ class SignIn extends React.Component {
               </Col>
               <Col lg={7}>
                 <div className="p-lg-5 p-3">
-                  <div className="text-center">
-                    <h1 className="h4 mb-4">Вход в систему!</h1>
+                  <div className="text-center mb-4">
+                    <Dropdown>
+                      <span className="text-middle">Вход в систему</span>
+                      <Dropdown.Toggle split variant="outline-primary" id="enter-dropdown"/>
+                      <Dropdown.Menu>
+                        <DropdownItem name="byLogin" onClick={this.switchSignInMethod}
+                                      disabled={!isEmailInput}>По логину</DropdownItem>
+                        <DropdownItem name="byEmail" onClick={this.switchSignInMethod}
+                                      disabled={isEmailInput}>По email</DropdownItem>
+                      </Dropdown.Menu>
+                    </Dropdown>
                   </div>
-                  <Form className="user">
-                    <Form.Group controlId="formGroupLoginEmail">
-                      <Form.Control className="form-control-user" type="email" placeholder="Логин/Email"/>
+
+                  <Form className="user" onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="formGroupLoginOrEmail">
+                      {emailOrLoginInput}
                     </Form.Group>
                     <Form.Group controlId="formGroupPassword">
-                      <Form.Control className="form-control-user" type="email" placeholder="Пароль"/>
+                      {passwordInput}
                     </Form.Group>
-                    <Button variant="outline-primary" className="btn-user" block>
+                    <Button type="submit" variant="outline-primary" className="btn-user" block>
                       Войти
                     </Button>
                   </Form>
+
                   <hr/>
                   <div className="text-center">
-                    <a className="small-text" href="#">
+                    <a className="text-small" href="#">
                       Забыли пароль?
                     </a>
                   </div>
                   <div className="text-center">
-                    <Link className="small-text" to="sign_up/">
+                    <Link className="text-small" to="sign_up/">
                       Еще не зарегистрированы? Вам сюда!
                     </Link>
                   </div>
@@ -54,7 +125,8 @@ class SignIn extends React.Component {
         </Card>
       </Container>
     )
-  };
+  }
+  ;
 }
 
 export default SignIn;
