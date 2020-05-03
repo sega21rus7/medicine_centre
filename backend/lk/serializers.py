a26d907d95bd1639aea98dcae9468b217dcb4065
sergeev-av <sega21rus98@mail.ru>
2020-05-03 16:30:20 +0300

    не работал импорт

--- backend/lk/serializers.py
+++ backend/lk/serializers.py
@@ -1,6 +1,5 @@
 from rest_auth.serializers import UserDetailsSerializer
 
-
 class CustomerUserSerializer(UserDetailsSerializer):
     class Meta(UserDetailsSerializer.Meta):
         fields = ('pk', 'username', 'email', 'first_name', 'last_name',
