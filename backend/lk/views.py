b7892bf4e882630239ae4d69af2c5dc6d74e575b
sergeev-av <sega21rus98@mail.ru>
2020-05-03 18:30:44 +0300

    django-rest вьюха для подтв email

---
+++ backend/lk/views.py
@@ -0,0 +1,30 @@
+from allauth.account.models import EmailConfirmationHMAC, EmailConfirmation
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
+from rest_framework.views import APIView
+
+
+class ConfirmEmailView(APIView):
+    def get(self, *args, **kwargs):
+        self.object = confirmation = self.get_object()
+        confirmation.confirm(self.request)
+        return redirect(reverse('rest_login'))
+
+    def get_object(self, queryset=None):
+        key = self.kwargs['key']
+        email_confirmation = EmailConfirmationHMAC.from_key(key)
+        if not email_confirmation:
+            if queryset is None:
+                queryset = self.get_queryset()
+            try:
+                email_confirmation = queryset.get(key=key.lower())
+            except EmailConfirmation.DoesNotExist:
+                # A React Router Route will handle the failure scenario
+                return HttpResponseRedirect('/login/failure/')
+        return email_confirmation
+
+    def get_queryset(self):
+        qs = EmailConfirmation.objects.all_valid()
+        qs = qs.select_related("email_address__user")
+        return qs
--- backend/medicine_centre/urls.py
+++ backend/medicine_centre/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_auth.registration.views import VerifyEmailView
 from rest_auth.views import PasswordResetView, PasswordResetConfirmView
+
+from lk.views import ConfirmEmailView
 
 urlpatterns = [
     path('lk/', include('lk.urls')),
@@ -12,15 +13,10 @@ urlpatterns = [
     path('client/', include('client.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-
+    path('rest-auth/registration/account-confirm-email/<key>/', ConfirmEmailView.as_view(),
+         name='account_confirm_email'),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('rest-auth/registration/account-confirm-email/<key>/', VerifyEmailView.as_view(),
-         name='account_confirm_email'),
-    path('rest-auth/password/reset/', PasswordResetView.as_view(),
-         name='password_reset'),
-    path('rest-auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
