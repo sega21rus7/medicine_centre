32f31c7519e11c0e7efd78c67ea7bb1515890b6c
sergeev-av <sega21rus98@mail.ru>
2020-05-05 16:00:24 +0300

    Валидация комментария

--- frontend/react-frontend/src/Forms/AddComment/AddComment.js
+++ frontend/react-frontend/src/Forms/AddComment/AddComment.js
@@ -4,12 +4,14 @@ import {Button, Container, Form} from "react-bootstrap";
 import CKEditor from "@ckeditor/ckeditor5-react"
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
 import axios from "axios";
+import ErrorValidateBlock from "../../Auth/ErrorValidateBlock/ErrorValidateBlock";
 
 class AddComment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comment: '',
+      comment: null,
+      errors: null,
     }
   }
 
@@ -29,10 +31,12 @@ class AddComment extends React.Component {
       axios(options)
         .then(response => {
           console.log(response.data);
+          this.setState({errors: null});
           this.props.getArticle();
         })
         .catch(error => {
           console.log(error.response.data);
+          this.setState({errors: error.response.data});
         });
     }
   };
@@ -42,16 +46,23 @@ class AddComment extends React.Component {
   };
 
   render() {
+    const {errors} = this.state;
+    if(errors){
+      var contentError = <ErrorValidateBlock text={errors.content}/>
+    }
+
     return (
       <Container className="AddComment">
         <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="formGroupComment">
             <CKEditor editor={ClassicEditor} name="comment" onChange={this.handleChange}/>
           </Form.Group>
+          {contentError}
           <Button type="submit" variant="primary">
             Опубликовать комментарий
           </Button>
         </Form>
+
       </Container>
     )
   };
