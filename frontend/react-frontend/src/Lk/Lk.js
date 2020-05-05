4a1d12b18035d6a92ab023c7c4c6431172b68e08
sergeev-av <sega21rus98@mail.ru>
2020-05-05 14:47:04 +0300

    не работает

--- frontend/react-frontend/src/Lk/Lk.js
+++ frontend/react-frontend/src/Lk/Lk.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Lk.css';
-import axios from "axios";
 
 
 class Lk extends React.Component {
@@ -9,15 +8,6 @@ class Lk extends React.Component {
     if (token) {
       token = JSON.parse(token);
       console.log(token);
-      axios.get('http://localhost:8000/rest-auth/user/',
-        {headers: {'Authorization': `Token ${token}`}})
-        .then(response => {
-          console.log(response.data);
-          localStorage.setItem('avatar', JSON.stringify(response.data.avatar));
-          localStorage.setItem('last_name', JSON.stringify(response.data.last_name));
-          localStorage.setItem('fist_name', JSON.stringify(response.data.first_name));
-          localStorage.setItem('middle_name', JSON.stringify(response.data.middle_name));
-        })
     }
   }
 
