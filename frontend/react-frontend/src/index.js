41625e33a11c5e9c4cfcc3ab659d7433984fabe2
sergeev-av <sega21rus98@mail.ru>
2020-05-06 15:50:37 +0300

    подключаем redux в App

--- frontend/react-frontend/src/index.js
+++ frontend/react-frontend/src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from "./App/App";
+import reducer from './store/reducers/auth';
+import {applyMiddleware, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+
+const composeEnHances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnHances(
+  applyMiddleware(thunk)
+));
+const app = (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+);
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+  app, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
