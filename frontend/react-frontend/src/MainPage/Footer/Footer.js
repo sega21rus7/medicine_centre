0f10d229416ec489b47f9baa943d5213db481eb0
sergeev-av <sega21rus98@mail.ru>
2020-05-06 12:38:33 +0300

    лишняя row в футере

--- frontend/react-frontend/src/MainPage/Footer/Footer.js
+++ frontend/react-frontend/src/MainPage/Footer/Footer.js
@@ -7,51 +7,49 @@ import {Link} from "react-router-dom";
 class Footer extends React.Component {
   render() {
     return (
-      <Container fluid className="footer bg-gradient-secondary">
-        <Row className="footer-widgets">
-          <Container>
-            <Row>
-              <Col lg={4}>
-                <div className="foot-about">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris
-                    scelerisque, at rutrum nulla dictum. Ut ac ligula sapien.</p>
-                  <p className="copyright">
-                    БУ «Медцентр» Минздрава Чувашии
-                    <br/>
-                    &copy;2020 Все права защищены
-                  </p>
-                </div>
-              </Col>
+      <div className="footer bg-gradient-secondary">
+        <Container>
+          <Row className="footer-widgets">
+            <Col lg={4}>
+              <div className="foot-about">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris
+                  scelerisque, at rutrum nulla dictum. Ut ac ligula sapien.</p>
+                <p className="copyright">
+                  БУ «Медцентр» Минздрава Чувашии
+                  <br/>
+                  &copy;2020 Все права защищены
+                </p>
+              </div>
+            </Col>
 
-              <Col lg={4} className="d-none d-lg-block">
-                <div className="foot-contact">
-                  <h2>Связаться с нами</h2>
+            <Col lg={4} className="d-none d-lg-block">
+              <div className="foot-contact">
+                <h2>Связаться с нами</h2>
 
-                  <ul className="p-0 m-0">
-                    <li><span>Адрес:</span>г. Чебоксары, ул. Пока не придумал, 3</li>
-                    <li><span>Телефон:</span>8 800 111-22-33</li>
-                    <li><span>Email:</span>chuvash_med_centre@gmail.com</li>
-                  </ul>
-                </div>
-              </Col>
+                <ul className="p-0 m-0">
+                  <li><span>Адрес:</span>г. Чебоксары, ул. Пока не придумал, 3</li>
+                  <li><span>Телефон:</span>8 800 111-22-33</li>
+                  <li><span>Email:</span>chuvash_med_centre@gmail.com</li>
+                </ul>
+              </div>
+            </Col>
 
-              <Col lg={4} className="d-none d-lg-block">
-                <div className="foot-links">
-                  <h2>Полезные ссылки</h2>
-                  <ul className="p-0 m-0">
-                    <li><Link to="/">Главная</Link></li>
-                    <li><Link to="/lk">Кабинет</Link></li>
-                    <li><Link to="/about_us">О нас</Link></li>
-                    <li><Link to="/departments">Отделения</Link></li>
-                    <li><Link to="/contacts">Контакты</Link></li>
-                    <li><Link to="/faq">FAQ</Link></li>
-                  </ul>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </Row>
-      </Container>
+            <Col lg={4} className="d-none d-lg-block">
+              <div className="foot-links">
+                <h2>Полезные ссылки</h2>
+                <ul className="p-0 m-0">
+                  <li><Link to="/">Главная</Link></li>
+                  <li><Link to="/lk">Кабинет</Link></li>
+                  <li><Link to="/about_us">О нас</Link></li>
+                  <li><Link to="/departments">Отделения</Link></li>
+                  <li><Link to="/contacts">Контакты</Link></li>
+                  <li><Link to="/faq">FAQ</Link></li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     )
   };
 }
