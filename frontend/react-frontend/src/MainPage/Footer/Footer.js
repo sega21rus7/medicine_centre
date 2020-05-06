542094dab825bbf8b58fad9c00f24b9d8732458b
sergeev-av <sega21rus98@mail.ru>
2020-05-06 12:30:38 +0300

    еще правки футера

--- frontend/react-frontend/src/MainPage/Footer/Footer.js
+++ frontend/react-frontend/src/MainPage/Footer/Footer.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import './Footer.css';
-import {Container, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 
 class Footer extends React.Component {
   render() {
     return (
-      <footer className="footer bg-gradient-secondary">
+      <Container fluid className="footer bg-gradient-secondary">
         <Row className="footer-widgets">
           <Container>
             <Row>
-              <div className="col-12 col-md-6 col-lg-4">
+              <Col lg={4}>
                 <div className="foot-about">
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris
                     scelerisque, at rutrum nulla dictum. Ut ac ligula sapien.</p>
-                  <p
-                    className="copyright">
+                  <p className="copyright">
                     БУ «Медцентр» Минздрава Чувашии
                     <br/>
                     &copy;2020 Все права защищены
                   </p>
                 </div>
-              </div>
+              </Col>
 
-              <div className="col-12 col-md-6 col-lg-4 mt-5 mt-md-0">
+              <Col lg={4} className="d-none d-lg-block">
                 <div className="foot-contact">
                   <h2>Связаться с нами</h2>
 
@@ -34,9 +33,9 @@ class Footer extends React.Component {
                     <li><span>Email:</span>chuvash_med_centre@gmail.com</li>
                   </ul>
                 </div>
-              </div>
+              </Col>
 
-              <div className="col-12 col-md-6 col-lg-4 mt-5 mt-md-0">
+              <Col lg={4} className="d-none d-lg-block">
                 <div className="foot-links">
                   <h2>Полезные ссылки</h2>
                   <ul className="p-0 m-0">
@@ -48,11 +47,11 @@ class Footer extends React.Component {
                     <li><Link to="/faq">FAQ</Link></li>
                   </ul>
                 </div>
-              </div>
+              </Col>
             </Row>
           </Container>
         </Row>
-      </footer>
+      </Container>
     )
   };
 }
