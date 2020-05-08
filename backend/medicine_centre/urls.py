70b8992751652ce0ae89298467859713a8481b41
sergeev-av <sega21rus98@mail.ru>
2020-05-08 12:51:22 +0300

    лишние импорты

--- backend/medicine_centre/urls.py
+++ backend/medicine_centre/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_auth.views import PasswordResetView, PasswordResetConfirmView
 
 from lk.views import ConfirmEmailView
 
