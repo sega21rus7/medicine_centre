6efbcfe11b7a1a4fc41f1d5c80cfd8213f39dfd6
sergeev-av <sega21rus98@mail.ru>
2020-05-08 12:53:10 +0300

    регистрируем новую модель в админке

--- backend/marketing/admin.py
+++ backend/marketing/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import News, Article, Tag, ArticleComment, Review, Feedback
+from .models import News, Article, Tag, ArticleComment, Review, Feedback, SupportQuestion
 
 admin.site.register(News)
 admin.site.register(Article)
@@ -8,3 +8,4 @@ admin.site.register(Tag)
 admin.site.register(ArticleComment)
 admin.site.register(Review)
 admin.site.register(Feedback)
+admin.site.register(SupportQuestion)
