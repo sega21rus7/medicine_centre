2dfc06812e2bb971c2dd12292be3c545ef04d8ee
sergeev-av <sega21rus98@mail.ru>
2020-05-08 15:53:11 +0300

    заготовка под google maps

---
+++ frontend/react-frontend/src/components/ContactsMap.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import {GoogleMap, withGoogleMap, withScriptjs} from "react-google-maps";
+//
+// class ContactsMap extends React.Component {
+//   render() {
+//     return (
+//       <GoogleMap
+//         defaultZoom={8}
+//         defaultCenter={{lat: -34.397, lng: 150.644}}
+//       >
+//         {/*{false && <Marker position={{lat: -34.397, lng: 150.644}}/>}*/}
+//       </GoogleMap>
+//     )
+//   };
+// }
+//
+// export default withGoogleMap(
+//   <ContactsMap
+//
+//   />);
+//
+// googleMapURL=
+//       "https://maps.googleapis.com/maps/api/js?key=APIKEY=3.exp&libraries=geometry,drawing,places"
+//     loadingElement={<div style={{height: `100%`}}/>}
+//     containerElement={<div style={{height: `400px`}}/>}
+//     mapElement={<div style={{height: `100%`}}/>}
+{/*<script type="text/javascript"*/}
+{/*        src=ссылка с ключом google api key 1$ для проверки с карты*/}
+
+{/*также https://www.youtube.com/watch?v=Pf7g32CwX_s&list=PLkgh1od3cdW6Rl8EaVnKkbhtiz-aXg7ta*/}
