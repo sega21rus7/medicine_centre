8495d2642f5de49b10ac967316bc0826e5a63182
sergeev-av <sega21rus98@mail.ru>
2020-05-08 20:19:11 +0300

    Профиль фронт

---
+++ frontend/react-frontend/src/components/ProfileForm.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import {Button, Col, Form, Row} from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import ErrorBlock from "./ErrorBlock/ErrorBlock";
+import axios from 'axios';
+
+class ProfileForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: null,
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let token = localStorage.getItem('token');
+    if (token) {
+      const elements = event.target.elements;
+      const options = {
+        method: 'PUT',
+        url: 'http://localhost:8000/rest-auth/user/',
+        data: {
+          username: elements.login.value,
+          email: elements.email.value,
+          phone_number: elements.phone.value,
+          first_name: elements.firstName.value,
+          last_name: elements.lastName.value,
+          middle_name: elements.middleName.value,
+          // avatar будет загружать в базу по новой, потом исправлю
+          avatar: elements.avatar.value || this.props.user.avatar,
+        },
+        headers: {'Authorization': `Token ${token}`},
+      };
+      axios(options)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err.response);
+          this.setState({errors: err.response.data});
+        })
+    }
+  };
+
+  render() {
+    const {user} = this.props;
+    const {errors} = this.state;
+    if (errors) {
+      var loginError = <ErrorBlock text={errors.login}/>;
+      var emailError = <ErrorBlock text={errors.email}/>;
+      var phoneError = <ErrorBlock text={errors.phone_number}/>;
+      var firstNameError = <ErrorBlock text={errors.first_name}/>;
+      var lastNameError = <ErrorBlock text={errors.last_name}/>;
+      var middleNameError = <ErrorBlock text={errors.middle_name}/>;
+      var avatarError = <ErrorBlock text={errors.avatar}/>;
+      var nonFieldErrors = <ErrorBlock text={errors.non_field_errors}/>;
+    }
+
+
+    return (
+      <Form className="user" onSubmit={this.handleSubmit}>
+        <Row>
+          <Col sm={4}>
+            <Form.Group controlId="formGroupLogin">
+              <Form.Label column="name">Логин</Form.Label>
+              <Form.Control className="form-control-user" type="text" name="login"
+                            defaultValue={user.username}/>
+              {loginError}
+            </Form.Group>
+            <Form.Group controlId="formGroupEmail">
+              <Form.Label column="name">Email</Form.Label>
+              <Form.Control className="form-control-user" type="email" name="email"
+                            defaultValue={user.email}/>
+              {emailError}
+            </Form.Group>
+            <Form.Group controlId="formGroupPhone">
+              <Form.Label column="name">Номер телефона</Form.Label>
+              <Form.Control className="form-control-user" type="text" name="phone"
+                            defaultValue={user.phone_number}/>
+              {phoneError}
+            </Form.Group>
+          </Col>
+
+          <Col sm={4}>
+            <Form.Group controlId="formGroupFirstName">
+              <Form.Label column="name">Имя</Form.Label>
+              <Form.Control className="form-control-user" type="text" name="firstName"
+                            defaultValue={user.first_name}/>
+              {firstNameError}
+            </Form.Group>
+            <Form.Group controlId="formGroupLastName">
+              <Form.Label column="name">Фамилия</Form.Label>
+              <Form.Control className="form-control-user" type="text" name="lastName"
+                            defaultValue={user.last_name}/>
+              {lastNameError}
+            </Form.Group>
+            <Form.Group controlId="formGroupMiddleName">
+              <Form.Label column="name">Отчество</Form.Label>
+              <Form.Control className="form-control-user" type="text" name="middleName"
+                            defaultValue={user.middle_name}/>
+              {middleNameError}
+            </Form.Group>
+          </Col>
+
+          <Col sm={4}>
+            <Form.Group controlId="formGroupAvatar">
+              <Form.Label column="name">Аватар</Form.Label>
+              {
+                user.avatar ?
+                  <Image
+                    src={user.avatar}
+                    alt="Аватар"
+                    className="img-fluid">
+                  </Image>
+                  :
+                  null
+              }
+              <Form.Control className="form-control-user" type="file" name="avatar"/>
+              {avatarError}
+            </Form.Group>
+          </Col>
+        </Row>
+        {nonFieldErrors}
+        <div className="text-right">
+          <Button type="submit" variant="outline-primary" className="btn-user">
+            Внести изменения
+          </Button>
+        </div>
+      </Form>
+    )
+  }
+  ;
+}
+
+export default ProfileForm;
--- frontend/react-frontend/src/containers/Lk.js
+++ frontend/react-frontend/src/containers/Lk.js
@@ -1,41 +1,63 @@
 import React from 'react';
 import {Col, Container, Nav, Row, Tab} from "react-bootstrap";
 import {connect} from "react-redux";
-
+import axios from "axios";
+import ProfileForm from '../components/ProfileForm'
 
 class Lk extends React.Component {
   constructor(props) {
     super(props);
-    this.patient_nav = [
+    this.state = {
+      user: {},
+    };
+    this.nav = [
       'Профиль',
       'Сменить пароль',
       'Мои отзывы',
-      'Записаться на прием',
       'История записей',
       'Задать вопрос',
+
     ];
-    this.doctor_nav = [
-      'Профиль',
-      'Сменить пароль',
-      'Мои пациенты',
-      'История записей',
-      'Мои отзывы',
-      'Задать вопрос',
-    ];
-    this.admin_nav = [
-      'Профиль',
-      'Сменить пароль',
-      'Пациенты',
-      'Врачи',
-      'История записей',
-      'Статьи',
-      'Новости',
-      'Обращения в ТП',
-      'Отзывы',
-    ];
+    // 'Записаться на прием',
+    //   'Мои пациенты',
+    // this.admin_nav = [
+    //   'Профиль',
+    //   'Сменить пароль',
+    //   'Пациенты',
+    //   'Врачи',
+    //   'История записей',
+    //   'Статьи',
+    //   'Новости',
+    //   'Обращения в ТП',
+    //   'Отзывы',
+    // ];
   }
 
+  componentDidMount() {
+    let token = localStorage.getItem('token');
+    if (token) {
+      const options = {
+        method: 'GET',
+        url: 'http://localhost:8000/rest-auth/user/',
+        headers: {'Authorization': `Token ${token}`},
+      };
+      axios(options)
+        .then(response => {
+          this.setState({user: response.data});
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
+    } else {
+      this.props.history.push('/sign_in');
+    }
+  };
+
   render() {
+    const {user} = this.state;
+    let userData = user ? user : null;
+
     return (
       <Container fluid className="Lk mt-4">
         <Tab.Container id="left-tabs" defaultActiveKey="0">
@@ -43,26 +65,21 @@ class Lk extends React.Component {
             <Col sm={3}>
 
               <Nav variant="pills" className="flex-column">
-                {
-                  this.patient_nav.map((item, index) => (
-                      <Nav.Item key={index} className="nav-item-bg-info">
-                        <Nav.Link eventKey={index}>{item}</Nav.Link>
-                      </Nav.Item>
-                    )
+                {this.nav.map((item, index) => (
+                    <Nav.Item key={index} className="nav-item-bg-info">
+                      <Nav.Link eventKey={index}>{item}</Nav.Link>
+                    </Nav.Item>
                   )
-                }
+                )}
               </Nav>
 
             </Col>
             <Col sm={9}>
               <Tab.Content>
                 {
-                  this.patient_nav.map((item, index) => (
-                      <Tab.Pane key={index} eventKey={index}>
-                        <p>{item}</p>
-                      </Tab.Pane>
-                    )
-                  )
+                  <Tab.Pane key="0" eventKey="0">
+                    <ProfileForm user={userData}/>
+                  </Tab.Pane>
                 }
               </Tab.Content>
             </Col>
