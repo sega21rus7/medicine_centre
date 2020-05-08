2e6f87bb0d20db958260169de01f7939568f96c1
sergeev-av <sega21rus98@mail.ru>
2020-05-08 13:48:05 +0300

    Простой шаблон будущего ЛК

--- frontend/react-frontend/src/containers/Lk.js
+++ frontend/react-frontend/src/containers/Lk.js
@@ -3,29 +3,66 @@ import {Col, Container, Nav, Row, Tab} from "react-bootstrap";
 
 
 class Lk extends React.Component {
+  constructor(props) {
+    super(props);
+    this.patient_nav = [
+      'Настройки профиля',
+      'Сменить пароль',
+      'Мои отзывы',
+      'Записаться на прием',
+      'История записей',
+      'Задать вопрос',
+    ];
+    this.doctor_nav = [
+      'Настройки профиля',
+      'Сменить пароль',
+      'Мои пациенты',
+      'История записей',
+      'Мои отзывы',
+      'Задать вопрос',
+    ];
+    this.admin_nav = [
+      'Настройки профиля',
+      'Сменить пароль',
+      'Пациенты',
+      'Врачи',
+      'История записей',
+      'Статьи',
+      'Новости',
+      'Обращения в ТП',
+      'Отзывы',
+    ];
+  }
+
   render() {
     return (
       <Container fluid className="Lk mt-4">
-        <Tab.Container id="left-tabs" defaultActiveKey="first">
+        <Tab.Container id="left-tabs" defaultActiveKey="0">
           <Row>
             <Col sm={3}>
+
               <Nav variant="pills" className="flex-column">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                </Nav.Item>
+                {
+                  this.patient_nav.map((item, index) => (
+                      <Nav.Item key={index} className="nav-item-bg-info">
+                        <Nav.Link eventKey={index}>{item}</Nav.Link>
+                      </Nav.Item>
+                    )
+                  )
+                }
               </Nav>
+
             </Col>
             <Col sm={9}>
               <Tab.Content>
-                <Tab.Pane eventKey="first">
-                  <p>test 1</p>
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">
-                  <p>test 2</p>
-                </Tab.Pane>
+                {
+                  this.patient_nav.map((item, index) => (
+                      <Tab.Pane eventKey={index}>
+                        <p>{item}</p>
+                      </Tab.Pane>
+                    )
+                  )
+                }
               </Tab.Content>
             </Col>
           </Row>
