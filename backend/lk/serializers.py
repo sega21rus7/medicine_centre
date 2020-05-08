bd74a6b33ebbc425bb98e5b7b14db57409d0edb7
sergeev-av <sega21rus98@mail.ru>
2020-05-08 18:29:29 +0300

    доп поля в user сериализаторе

--- backend/lk/serializers.py
+++ backend/lk/serializers.py
@@ -1,7 +1,8 @@
 from rest_auth.serializers import UserDetailsSerializer
 
+
 class CustomerUserSerializer(UserDetailsSerializer):
     class Meta(UserDetailsSerializer.Meta):
         fields = ('pk', 'username', 'email', 'first_name', 'last_name',
-                  'middle_name', 'phone_number', 'avatar',)
+                  'middle_name', 'phone_number', 'avatar', 'doctor', 'patient', 'nurse', 'is_superuser')
         read_only_fields = ()
