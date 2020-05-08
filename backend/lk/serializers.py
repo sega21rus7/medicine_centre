e3e7b754c6ff8106496cd929519affe518d65f1b
sergeev-av <sega21rus98@mail.ru>
2020-05-08 20:02:47 +0300

    опечатка

--- backend/lk/serializers.py
+++ backend/lk/serializers.py
@@ -5,4 +5,4 @@ class CustomerUserSerializer(UserDetailsSerializer):
     class Meta(UserDetailsSerializer.Meta):
         fields = ('pk', 'username', 'email', 'first_name', 'last_name',
                   'middle_name', 'phone_number', 'avatar', 'doctor', 'patient', 'nurse', 'is_superuser')
-        read_only_fields = ('doctor', 'patient', 'nurse', 'is_super_user')
+        read_only_fields = ('doctor', 'patient', 'nurse', 'is_superuser')
