284933bf89d741b9a7f1d5548af1ec9fc745c618
sergeev-av <sega21rus98@mail.ru>
2020-05-09 12:07:17 +0300

    компонент подтверждения email

--- frontend/react-frontend/src/containers/Auth/VerifyEmail.js
+++ frontend/react-frontend/src/containers/Auth/VerifyEmail.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from "axios";
-import {Redirect} from "react-router";
+import {Card, Col, Container, Image, Row} from "react-bootstrap";
+import image from "./sign_image.jpg";
+import {Link} from "react-router-dom";
 
 
 class VerifyEmail extends React.Component {
@@ -28,10 +30,41 @@ class VerifyEmail extends React.Component {
       message = 'При подтверждении email произошла ошибка!';
     }
     return (
-      <Redirect to={{
-        pathname: '/sign_in',
-        state: {message: message}
-      }}/>
+      <Container className="VerifyEmail">
+        <Card className="o-hidden border-0 shadow-lg mt-4">
+          <Card.Body className="p-0">
+            <Row>
+              <Col lg={6} className="d-none d-lg-block bg-image">
+                <Image
+                  src={image}
+                  alt=""
+                  className="img-fluid"
+                >
+                </Image>
+              </Col>
+              <Col lg={6}>
+                <div className="p-lg-5 p-3">
+                  <div className="text-center">
+                    <h1 className="caption-center">Подтверждение email</h1>
+                    <p>{message}</p>
+                    <hr/>
+                    <div className="text-center">
+                      <Link className="text-small" to="/forgot_password">
+                        Забыли пароль?
+                      </Link>
+                    </div>
+                    <div className="text-center">
+                      <Link className="text-small" to="/sign_in">
+                        Уже есть аккаунт? Войдите
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
     )
   };
 }
