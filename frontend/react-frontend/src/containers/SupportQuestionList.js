3695a676269ae62eb1d96ca9e5a640348d8adb5d
sergeev-av <sega21rus98@mail.ru>
2020-05-10 09:25:42 +0300

    шаблон запроса в тп

---
+++ frontend/react-frontend/src/containers/SupportQuestionList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+
+class SupportQuestionList extends React.Component {
+  render() {
+    return (
+      <Container className="SupportQuestionList">
+
+      </Container>
+    )
+  };
+}
+
+export default SupportQuestionList;
