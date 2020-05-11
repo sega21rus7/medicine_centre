38b3224662c141022b499269c7fff6c26c2acd64
sergeev-av <sega21rus98@mail.ru>
2020-05-11 17:43:16 +0300

    правки в моделях

--- backend/staff/models.py
+++ backend/staff/models.py
@@ -46,26 +46,6 @@ class QualificationCategory(ReferenceByNameModel):
         verbose_name_plural = 'Квалификационные категории'
 
 
-class Award(ReferenceByNameModel):
-    image = smart_fields.ImageField(verbose_name='Изображение', upload_to='staff/images/awards',
-                                    null=True, blank=True)
-    doctor = models.ForeignKey(Post, verbose_name='Врач', on_delete=models.CASCADE)
-
-    class Meta(ReferenceByNameModel.Meta):
-        verbose_name = 'Награда'
-        verbose_name_plural = 'Награды'
-
-
-class Certificate(ReferenceByNameModel):
-    image = smart_fields.ImageField(verbose_name='Изображение', upload_to='staff/images/certificates',
-                                    null=True, blank=True)
-    doctor = models.ForeignKey(Post, verbose_name='Врач', on_delete=models.CASCADE)
-
-    class Meta(ReferenceByNameModel.Meta):
-        verbose_name = 'Сертификат'
-        verbose_name_plural = 'Сертификаты'
-
-
 class Doctor(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name='Пользователь',
                                 on_delete=models.CASCADE)
@@ -80,7 +60,7 @@ class Doctor(models.Model):
     #                              blank=True, null=True)
     university = models.ForeignKey(University, verbose_name='Высшее учебное заведение',
                                    on_delete=models.CASCADE)
-    diploma_specialty = models.ForeignKey(University, verbose_name='Специальность по диплому',
+    diploma_specialty = models.ForeignKey(DiplomaSpecialty, verbose_name='Специальность по диплому',
                                           on_delete=models.CASCADE, blank=True, null=True)
     qualification_Category = models.ForeignKey(QualificationCategory,
                                                verbose_name='Квалификационная категория',
@@ -97,3 +77,23 @@ class Doctor(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.user.get_fio() or self.user.username)
         super().save(*args, **kwargs)
+
+
+class Award(ReferenceByNameModel):
+    image = smart_fields.ImageField(verbose_name='Изображение', upload_to='staff/images/awards',
+                                    null=True, blank=True)
+    doctor = models.ForeignKey(Doctor, verbose_name='Врач', on_delete=models.CASCADE)
+
+    class Meta(ReferenceByNameModel.Meta):
+        verbose_name = 'Награда'
+        verbose_name_plural = 'Награды'
+
+
+class Certificate(ReferenceByNameModel):
+    image = smart_fields.ImageField(verbose_name='Изображение', upload_to='staff/images/certificates',
+                                    null=True, blank=True)
+    doctor = models.ForeignKey(Doctor, verbose_name='Врач', on_delete=models.CASCADE)
+
+    class Meta(ReferenceByNameModel.Meta):
+        verbose_name = 'Сертификат'
+        verbose_name_plural = 'Сертификаты'
