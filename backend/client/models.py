2cd924fa610911caf80caf3dc924119e41f46327
sergeev-av <sega21rus98@mail.ru>
2020-05-11 10:35:16 +0300

    правки моделей пациента и врача

--- backend/client/models.py
+++ backend/client/models.py
@@ -2,36 +2,9 @@ from django.conf import settings
 from django.db import models
 
 
-class Passport(models.Model):
-    series = models.CharField(verbose_name='Серия', max_length=4, db_index=True)
-    number = models.CharField(verbose_name='Номер', max_length=6, db_index=True)
-
-    class Meta:
-        verbose_name = 'Паспорт'
-        verbose_name_plural = 'Паспорта'
-
-    def __str__(self):
-        return '%s - %s' % (self.series, self.number)
-
-
-class InsurancePolicy(models.Model):
-    number = models.CharField(verbose_name='Номер', max_length=16, db_index=True)
-
-    class Meta:
-        verbose_name = 'Полис ОМС'
-        verbose_name_plural = 'Полисы ОМС'
-        ordering = ('-pk',)
-
-    def __str__(self):
-        return str(self.number)
-
-
 class Patient(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name='Пользователь',
                                 on_delete=models.CASCADE)
-    passport = models.OneToOneField(Passport, verbose_name='Паспорт', on_delete=models.CASCADE)
-    insurance_policy = models.OneToOneField(InsurancePolicy, verbose_name='Полис ОМС',
-                                            on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Пациент'
--- backend/staff/models.py
+++ backend/staff/models.py
@@ -1,7 +1,18 @@
 from django.conf import settings
 from django.db import models
-from django.urls import reverse
 from pytils.translit import slugify
+
+
+class Department(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=150, db_index=True)
+
+    class Meta:
+        verbose_name = 'Отделение'
+        verbose_name_plural = 'Отделения'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -21,6 +32,8 @@ class Employee(models.Model):
                                 on_delete=models.CASCADE)
     post = models.ForeignKey(Post, verbose_name='Должность', on_delete=models.CASCADE)
     slug = models.SlugField(max_length=150, unique=True, blank=True)
+    experience = models.IntegerField(verbose_name='Стаж работы', max_length=2)
+    department = models.ForeignKey(Department, verbose_name='Отделение', on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
@@ -31,9 +44,6 @@ class Employee(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.user.get_fio() or self.user.username)
         super().save(*args, **kwargs)
-
-    def get_absolute_url(self):
-        return reverse("staff:doctor_detail", kwargs={"slug": self.slug})
 
 
 class Doctor(Employee):
