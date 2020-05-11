c7c1c10e06eae7dd1afd989220ef2143bb23b276
sergeev-av <sega21rus98@mail.ru>
2020-05-11 19:12:58 +0300

    начинаем сериализовывать наши новые модели

--- backend/staff/serializers.py
+++ backend/staff/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from lk.serializers import CustomerUserSerializer
-from .models import Doctor, Post
+from .models import Doctor, Post, Department, University, QualificationCategory, DiplomaSpecialty, Award, Certificate
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -10,16 +10,54 @@ class PostSerializer(serializers.ModelSerializer):
         fields = ('name',)
 
 
-class DoctorListSerializer(serializers.ModelSerializer):
-    user = CustomerUserSerializer(read_only=True)
-    post = PostSerializer(read_only=True)
-
+class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Doctor
-        fields = ('user', 'post', 'slug',)
+        model = Department
+        fields = ('name',)
+
+
+class UniversitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = University
+        fields = ('name',)
+
+
+class DiplomaSpecialtySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DiplomaSpecialty
+        fields = ('name',)
+
+
+class QualificationCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QualificationCategory
+        fields = ('name',)
+
+
+class AwardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Award
+        fields = ('name', 'image',)
+
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ('name', 'image',)
 
 
 class DoctorCreateUpdateDestroySerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ('user', 'post',)
+        fields = ('user', 'post', 'slug', 'experience_from', 'department', 'additional_education',
+                  'university', 'diploma_specialty', 'qualification_category',
+                  'awards', 'certificates')
+
+
+class DoctorListSerializer(DoctorCreateUpdateDestroySerializer):
+    user = CustomerUserSerializer()
+    post = PostSerializer()
+    department = DepartmentSerializer()
+    university = UniversitySerializer()
+    diploma_specialty = DiplomaSpecialtySerializer()
+    qualification_category = QualificationCategorySerializer()
