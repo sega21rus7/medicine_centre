c800e8d5388d150f316979c1292a8ce048097d11
sergeev-av <sega21rus98@mail.ru>
2020-05-11 10:37:48 +0300

    регистрируем изменения в админке

--- backend/client/admin.py
+++ backend/client/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import Passport, InsurancePolicy, Patient
+from .models import Patient
 
-admin.site.register(Passport)
-admin.site.register(InsurancePolicy)
 admin.site.register(Patient)
--- backend/reception/admin.py
+++ backend/reception/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
 
+from .models import Reception, Office
+
+admin.site.register(Reception)
+admin.site.register(Office)
