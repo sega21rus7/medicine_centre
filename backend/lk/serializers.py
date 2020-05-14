a19dac367a430bdb289fff7fc6fc43cb5fa50bfc
sergeev-av <sega21rus98@mail.ru>
2020-05-14 13:29:39 +0300

    сериализатор для почт привязанных к аккаунту

--- backend/lk/serializers.py
+++ backend/lk/serializers.py
@@ -1,17 +1,27 @@
+from allauth.account.models import EmailAddress
 from django.contrib.auth import get_user_model
-from rest_auth import serializers
 from rest_auth.serializers import UserDetailsSerializer as RestCustomerUserSerializer, \
-    PasswordResetSerializer as RestPasswordResetSerializer
+    PasswordResetSerializer as RestPasswordResetSerializer, \
+    ValidationError
+from rest_framework import serializers
 
 from medicine_centre import settings
 
 User = get_user_model()
 
 
+class EmailAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmailAddress
+        fields = ('email', 'verified', 'primary',)
+
+
 class CustomerUserSerializer(RestCustomerUserSerializer):
+    emailaddress_set = EmailAddressSerializer(many=True)
+
     class Meta(RestCustomerUserSerializer.Meta):
         fields = ('pk', 'username', 'email', 'first_name', 'last_name',
-                  'middle_name', 'phone_number', 'avatar', 'doctor', 'patient', 'is_superuser')
+                  'middle_name', 'phone_number', 'avatar', 'doctor', 'patient', 'is_superuser', 'emailaddress_set')
         read_only_fields = ('doctor', 'patient', 'is_superuser')
 
 
@@ -19,7 +29,7 @@ class PasswordResetSerializer(RestPasswordResetSerializer):
     def validate_email(self, value):
         value = super(PasswordResetSerializer, self).validate_email(value)
         if not User.objects.filter(email__iexact=value, is_active=True).exists():
-            raise serializers.ValidationError('Пользователя с таким email не существует')
+            raise ValidationError('Пользователя с таким email не существует')
         return value
 
     def get_email_options(self):
