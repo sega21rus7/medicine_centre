e3b10cf25652bf4555a8b33157e7a8e70fe4eff4
sergeev-av <sega21rus98@mail.ru>
2020-05-15 12:07:00 +0300

    правка названия переменной

--- frontend/react-frontend/src/index.js
+++ frontend/react-frontend/src/index.js
@@ -8,8 +8,8 @@ import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 
-const composeEnHances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnHances(
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhances(
   applyMiddleware(thunk)
 ));
 const app = (
