4778ec97ce15bd39e7e5db40ad0dbe001bab6167
sergeev-av <sega21rus98@mail.ru>
2020-05-15 13:18:08 +0300

    error404 по-русски

--- frontend/react-frontend/src/containers/Error404.js
+++ frontend/react-frontend/src/containers/Error404.js
@@ -5,7 +5,7 @@ class Error404 extends React.Component {
   render() {
     return (
       <div className="Error404">
-        <h1 className="orange-caption-center mt-4">Error 404</h1>
+        <h1 className="orange-caption-center mt-4">Упс... Такой страницы не существует</h1>
       </div>
     )
   };
