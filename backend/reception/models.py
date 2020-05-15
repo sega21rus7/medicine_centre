97a66b542167a00ea20e2bb8d4e1b16ae1df70f5
sergeev-av <sega21rus98@mail.ru>
2020-05-15 12:46:33 +0300

    пока комментируем, нужно пересмотреть модели

--- backend/reception/models.py
+++ backend/reception/models.py
@@ -1,29 +1,29 @@
-from django.db import models
+# from django.db import models
+#
+#
+# class Office(models.Model):
+#     number = models.CharField(verbose_name='Номер', max_length=3)
+#     floor = models.CharField(verbose_name='Этаж', max_length=1)
+#
+#     class Meta:
+#         verbose_name = 'Кабинет'
+#         verbose_name_plural = 'Кабинеты'
+#
+#     def __str__(self):
+#         return '№ %s' % self.number
 
 
-class Office(models.Model):
-    number = models.CharField(verbose_name='Номер', max_length=3)
-    floor = models.CharField(verbose_name='Этаж', max_length=1)
-
-    class Meta:
-        verbose_name = 'Кабинет'
-        verbose_name_plural = 'Кабинеты'
-
-    def __str__(self):
-        return '№ %s' % self.number
-
-
-class Reception(models.Model):
-    patient = models.ForeignKey('client.Patient', verbose_name='Пациент',
-                                on_delete=models.CASCADE, related_name='receptions')
-    doctor = models.ForeignKey('staff.Doctor', verbose_name='Врачи',
-                               on_delete=models.CASCADE, related_name='receptions')
-    date = models.DateField(verbose_name='Дата')
-    time = models.TimeField(verbose_name='Время')
-
-    class Meta:
-        verbose_name = 'Прием'
-        verbose_name_plural = 'Приемы'
-
-    def __str__(self):
-        return str(self.pk)
+# class Reception(models.Model):
+#     patient = models.ForeignKey('client.Patient', verbose_name='Пациент',
+#                                 on_delete=models.CASCADE, related_name='receptions')
+#     doctor = models.ForeignKey('staff.Doctor', verbose_name='Врачи',
+#                                on_delete=models.CASCADE, related_name='receptions')
+#     date = models.DateField(verbose_name='Дата')
+#     time = models.TimeField(verbose_name='Время')
+#
+#     class Meta:
+#         verbose_name = 'Прием'
+#         verbose_name_plural = 'Приемы'
+#
+#     def __str__(self):
+#         return str(self.pk)
