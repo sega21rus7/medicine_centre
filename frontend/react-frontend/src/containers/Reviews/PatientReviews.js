114971cc874e18a1cadc04b390d7e45ca2a53fd3
sergeev-av <sega21rus98@mail.ru>
2020-05-19 14:44:40 +0300

    табы для отзывов пациента

--- frontend/react-frontend/src/containers/Reviews/PatientReviews.js
+++ frontend/react-frontend/src/containers/Reviews/PatientReviews.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import {Container, Tab, Tabs} from "react-bootstrap";
 import AddReviewForm from "../../components/Reviews/AddReviewForm";
 import ReviewLongList from "./ReviewLongList";
 
@@ -8,17 +8,17 @@ class PatientReviews extends React.Component {
   render() {
     return (
       <Container className="PatientReview">
-        <Row>
-          <Col lg={6}>
+        <Tabs defaultActiveKey="view" id="tab-patient-reviews">
+          <Tab eventKey="view" title="Просмотр">
             <ReviewLongList personalTitle={'Мои отзывы'}
                             personalUrl={'http://localhost:8000/marketing/api/patient_reviews/'}
                             isNotMt4={true}
                             isChangeable={true}/>
-          </Col>
-          <Col lg={6}>
+          </Tab>
+          <Tab eventKey="add" title="Добавить">
             <AddReviewForm/>
-          </Col>
-        </Row>
+          </Tab>
+        </Tabs>
       </Container>
     )
   };
