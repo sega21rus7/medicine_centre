d4b54e430e973b477c08ac0ba56e0383ead36c37
sergeev-av <sega21rus98@mail.ru>
2020-05-20 15:21:33 +0300

    не работает

--- frontend/react-frontend/src/containers/MakeAppointment.js
+++ frontend/react-frontend/src/containers/MakeAppointment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from "react-redux";
 import axios from "axios";
+import {Table} from "react-bootstrap";
 
 class MakeAppointment extends React.Component {
   constructor(props) {
@@ -10,11 +11,11 @@ class MakeAppointment extends React.Component {
     };
   }
 
-  static getDerivedStateFromProps(props, state) {
+  componentDidMount() {
     const token = localStorage.getItem('token');
     let schedule = {};
-    if (token && props.user) {
-      const pk = props.user.doctor;
+    if (token && this.props.user) {
+      const pk = this.props.user.doctor;
       const url = `http://localhost:8000/staff/api/work_times_by_doctor/${pk}`;
       const options = {
         method: 'GET',
@@ -23,22 +24,44 @@ class MakeAppointment extends React.Component {
       };
       axios(options)
         .then(res => {
-          schedule = res.data;
+          this.setState({schedule: res.data});
           console.log(res.data);
         })
         .catch(err => {
           console.log(err.response);
         });
     }
-    return {schedule: schedule};
   }
 
   render() {
     const {schedule} = this.state;
-
+    console.log(schedule);
     return (
       <div className="MakeAppointment">
+        <Table bordered>
+          <thead>
+          <tr>
+            <th>Дата</th>
+            <th>С</th>
+            <th>По</th>
+            <th>Занято</th>
+          </tr>
+          </thead>
+          <tbody>
 
+          {/*{*/}
+          {/*  schedule.map((item, index) => (*/}
+          {/*    <tr>*/}
+          {/*      <td>1</td>*/}
+          {/*      <td>Mark</td>*/}
+          {/*      <td>Otto</td>*/}
+          {/*      <td>@mdo</td>*/}
+          {/*    </tr>*/}
+          {/*  ))*/}
+          {/*}*/}
+
+          </tbody>
+        </Table>
       </div>
     )
   }
