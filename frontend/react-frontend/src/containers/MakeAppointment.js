71d64af0969077a47a8d4a09ea009c3dba0a29e0
sergeev-av <sega21rus98@mail.ru>
2020-05-20 14:37:01 +0300

    шаблон

--- frontend/react-frontend/src/containers/MakeAppointment.js
+++ frontend/react-frontend/src/containers/MakeAppointment.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import {connect} from "react-redux";
+import axios from "axios";
 
 class MakeAppointment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      schedule: {},
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const token = localStorage.getItem('token');
+    let schedule = {};
+    if (token && props.user) {
+      const pk = props.user.doctor;
+      const url = `http://localhost:8000/staff/api/work_times_by_doctor/${pk}`;
+      const options = {
+        method: 'GET',
+        url: url,
+        headers: {'Authorization': `Token ${token}`},
+      };
+      axios(options)
+        .then(res => {
+          schedule = res.data;
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err.response);
+        });
+    }
+    return {schedule: schedule};
+  }
+
   render() {
+    const {schedule} = this.state;
+
     return (
       <div className="MakeAppointment">
 
@@ -10,4 +44,10 @@ class MakeAppointment extends React.Component {
   }
 }
 
-export default MakeAppointment;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  }
+};
+
+export default connect(mapStateToProps, null)(MakeAppointment);
