23816a20c7cd776650ee5fb970ebf2c4c5c895bc
sergeev-av <sega21rus98@mail.ru>
2020-05-24 17:50:39 +0300

    вьюха для receptions по доктору DRF

--- backend/reception/urls.py
+++ backend/reception/urls.py
@@ -1,9 +1,16 @@
+from oauthlib.uri_validate import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ReceptionViewSet
+from .views import ReceptionViewSet, ReceptionByDoctorListView
 
 app_name = 'reception'
 
+urlpatterns = [
+    path('api/receptions_by_doctor/<doctor_pk>/', ReceptionByDoctorListView.as_view(),
+         name='receptions_by_doctor'),
+]
+
 router = DefaultRouter()
+
 router.register(r'api/receptions', ReceptionViewSet, basename='receptions')
-urlpatterns = router.urls
+urlpatterns += router.urls
--- backend/reception/views.py
+++ backend/reception/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from medicine_centre.paginators import StandardPagination
@@ -13,3 +14,14 @@ class ReceptionViewSet(MultipleSerializerViewSetMixin, viewsets.ModelViewSet):
     crud_serializer_class = ReceptionCreateUpdateDestroySerializer
     permission_classes = (IsAuthenticated,)
     pagination_class = StandardPagination
+
+
+class ReceptionByDoctorListView(ListAPIView):
+    serializer_class = ReceptionListSerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = StandardPagination
+
+    def get_queryset(self):
+        doctor_pk = self.kwargs['doctor_pk']
+        qs = Reception.objects.filter(doctor_id=doctor_pk)
+        return qs
