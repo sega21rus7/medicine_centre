350fad6aa7d57cfafe0ac0568a4eb6ea3ac731b3
sergeev-av <sega21rus98@mail.ru>
2020-06-12 19:43:07 +0300

    берем статику из staticfiles

--- backend/medicine_centre/settings.py
+++ backend/medicine_centre/settings.py
@@ -147,8 +147,7 @@ DATE_FORMAT = 'd E Y'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
