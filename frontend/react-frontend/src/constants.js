ee3542883429f028df17fda0b1717c656fa9a5ce
sergeev-av <sega21rus98@mail.ru>
2020-06-13 13:22:45 +0300

    email изменился

--- frontend/react-frontend/src/constants.js
+++ frontend/react-frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const COMPANY_EMAIL = 'chuvash_med_centre@gmail.com';
+export const COMPANY_EMAIL = 'chuvash-medicine-centre@mail.ru';
 export const COMPANY_PHONE = '8 800 111-22-33';
 export const COMPANY_ADDRESS = 'г. Чебоксары, ул. Карла Маркса, 36';
 export const GOOGLE_API_KEY = 'AIzaSyBRrUrAMvwhI_oyn3tGfgQjcCATSzXHIaY';
