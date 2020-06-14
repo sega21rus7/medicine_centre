062adae513e2ab0474d6502a3facf5805aa2b340
sergeev-av <sega21rus98@mail.ru>
2020-06-14 14:38:52 +0300

    правка url для посылки запросов к rest api

--- frontend/react-frontend/src/constants.js
+++ frontend/react-frontend/src/constants.js
@@ -2,4 +2,4 @@ export const COMPANY_EMAIL = 'chuvash-medicine-centre@mail.ru';
 export const COMPANY_PHONE = '8 800 111-22-33';
 export const COMPANY_ADDRESS = 'г. Чебоксары, ул. Карла Маркса, 36';
 export const GOOGLE_API_KEY = 'AIzaSyBRrUrAMvwhI_oyn3tGfgQjcCATSzXHIaY';
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
+export const BACKEND_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000';
