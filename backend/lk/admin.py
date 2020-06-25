a84bb5145891aadbf06ebe384323bee0ec3ee388
sega21rus7 <sega21rus98@mail.ru>
2020-06-25 10:41:54 +0300

    шифрование пароля в админке не работало при создании юзера

--- backend/lk/admin.py
+++ backend/lk/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import CustomerUser
 
 
-class CustomerUserAdmin(admin.ModelAdmin):
+class CustomerUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'),
+         {'fields': ('first_name', 'last_name', 'middle_name', 'email', 'phone_number', 'avatar', 'role')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
     list_display = ('username', 'email', 'last_name', 'first_name', 'middle_name', 'role')
 
 
